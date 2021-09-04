3825248cfa9e63fce12f3542e1f3f00c593ffa97
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-09-04 17:09:04 +0100

    Adding in Delete Functionality

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -12,6 +12,10 @@ export interface FetchTodosAction {
   type: ActionTypes.fetchTodos;
   payload: Todo[];
 }
+export interface DeleteTodoAction {
+  type: ActionTypes.deleteTodo;
+  payload: number;
+}
 
 const url = "https://jsonplaceholder.typicode.com/todos";
 
@@ -23,5 +27,12 @@ export const fetchTodos = () => {
       type: ActionTypes.fetchTodos,
       payload: response.data,
     });
+  };
+};
+
+export const deleteTodo = (id: number): DeleteTodoAction => {
+  return {
+    type: ActionTypes.deleteTodo,
+    payload: id,
   };
 };
