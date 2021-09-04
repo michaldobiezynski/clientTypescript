37a11380b910a20534b82f04c53c1178c7beee62
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-09-04 20:04:26 +0100

    Expressing Actions as Type Union

--- src/reducers/todos.ts
+++ src/reducers/todos.ts
@@ -1,8 +1,10 @@
-import { Todo, FetchTodosAction, ActionTypes } from "./../actions/index";
+import { Todo, Action, ActionTypes } from "../actions";
 
-export const todosReducer = (state: Todo[] = [], action: FetchTodosAction) => {
+export const todosReducer = (state: Todo[] = [], action: Action) => {
   switch (action.type) {
     case ActionTypes.fetchTodos:
+      return action.payload;
+    case ActionTypes.deleteTodo:
       return action.payload;
     default:
       return state;
